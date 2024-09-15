9a6789c0ec65acf201cd49564d2a8f1ebe2b1782
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-09-15 03:38:35 +0300

    feat: add filter functionality and handle it

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const addButton = document.querySelector(".todo__button");
 const inputElement = document.querySelector(".todo__input");
 const todoList = document.querySelector(".todo-list");
+const filterOptions = document.querySelector(".filter-todo");
 
 //Event Listeners
 addButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", removeCheeck);
-
+filterOptions.addEventListener("click", filterTodo);
 //functions
 
 function addTodo(event) {
@@ -55,4 +56,30 @@ function removeCheeck(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+
+  todos.forEach((todo) => {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "none";
+        } else {
+          todo.style.display = "flex";
+        }
+        break;
+    }
+  });
 }
